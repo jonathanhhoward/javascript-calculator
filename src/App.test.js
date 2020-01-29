4bee9f700b38714cd62164ba72b7a8100dc2882d
slipskid <2773262@gmail.com>
2020-01-28 19:33:25 -0600

    Add 'rejects a leading zero'

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App/>, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('rejects a leading zero', () => {
+  const { getByTestId, getAllByRole } = render(<App/>)
+  const buttons = getAllByRole('button')
+  const zeroButton = buttons.find(node => node.textContent === '0')
+  const oneButton = buttons.find(node => node.textContent === '1')
+  const display = getByTestId('display')
+
+  fireEvent.click(zeroButton)
+  fireEvent.click(oneButton)
+
+  expect(display).toHaveTextContent(/^1$/)
 })
