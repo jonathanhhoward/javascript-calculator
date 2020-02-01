2ee10db304b1276565c61fc20328f450bf30c31c
slipskid <2773262@gmail.com>
2020-02-01 11:05:48 -0500

    Move tests into describes

--- src/App.test.js
+++ src/App.test.js
@@ -4,91 +4,97 @@ import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import App from './App'
 
-let display
-let keyPad
-
-beforeEach(() => {
-  const { getByTestId, getAllByRole } = render(<App/>)
-  const EXPRESSION = getByTestId('expression')
-  const INPUT = getByTestId('input')
-  const buttons = getAllByRole('button')
-  const [
-    CLEAR, DELETE, DIVIDE, MULTIPLY, SUBTRACT, ADD, EQUALS, DECIMAL, ZERO,
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
-  ] = buttons
-
-  display = { EXPRESSION, INPUT }
-  keyPad = {
-    CLEAR, DELETE, DIVIDE, MULTIPLY, SUBTRACT, ADD, EQUALS, DECIMAL,
-    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
-  }
-})
-
 test('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App/>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
 
-test('prevents leading zeros', () => {
-  fireEvent.click(keyPad.ZERO)
-  fireEvent.click(keyPad.ONE)
+describe('display on key press', () => {
+  let display
+  let keyPad
 
-  expect(display.EXPRESSION).toHaveTextContent(/^1$/)
-  expect(display.INPUT).toHaveTextContent(/^1$/)
-})
+  beforeEach(() => {
+    const { getByTestId, getAllByRole } = render(<App/>)
+    const EXPRESSION = getByTestId('expression')
+    const INPUT = getByTestId('input')
+    const buttons = getAllByRole('button')
+    const [
+      CLEAR, DELETE, DIVIDE, MULTIPLY, SUBTRACT, ADD, EQUALS, DECIMAL, ZERO,
+      ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+    ] = buttons
 
-test('prevents multiple decimals', () => {
-  fireEvent.click(keyPad.DECIMAL)
-  fireEvent.click(keyPad.DECIMAL)
+    display = { EXPRESSION, INPUT }
+    keyPad = {
+      CLEAR, DELETE, DIVIDE, MULTIPLY, SUBTRACT, ADD, EQUALS, DECIMAL,
+      ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+    }
+  })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
-  expect(display.INPUT).toHaveTextContent(/^0\.$/)
-})
+  describe('operators', () => {
+    test('uses last operator clicked', () => {
+      fireEvent.click(keyPad.ONE)
+      fireEvent.click(keyPad.MULTIPLY)
+      fireEvent.click(keyPad.ADD)
 
-test('prepends decimal with zero', () => {
-  fireEvent.click(keyPad.DECIMAL)
+      expect(display.EXPRESSION).toHaveTextContent(/^1\+$/)
+      expect(display.INPUT).toHaveTextContent(/^\+$/)
+    })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
-  expect(display.INPUT).toHaveTextContent(/^0\.$/)
+    test('uses negative on operator followed by minus', () => {
+      fireEvent.click(keyPad.ONE)
+      fireEvent.click(keyPad.MULTIPLY)
+      fireEvent.click(keyPad.SUBTRACT)
 
-  fireEvent.click(keyPad.ADD)
-  fireEvent.click(keyPad.DECIMAL)
+      expect(display.EXPRESSION).toHaveTextContent(/^1\*-$/)
+      expect(display.INPUT).toHaveTextContent(/^-$/)
+    })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^0\.\+0\.$/)
-  expect(display.INPUT).toHaveTextContent(/^0\.$/)
+    test('uses subtraction on negative followed by minus', () => {
+      fireEvent.click(keyPad.ONE)
+      fireEvent.click(keyPad.MULTIPLY)
+      fireEvent.click(keyPad.SUBTRACT)
+      fireEvent.click(keyPad.SUBTRACT)
 
-  fireEvent.click(keyPad.EQUALS)
-  fireEvent.click(keyPad.DECIMAL)
+      expect(display.EXPRESSION).toHaveTextContent(/^1-$/)
+      expect(display.INPUT).toHaveTextContent(/^-$/)
+    })
+  })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
-  expect(display.INPUT).toHaveTextContent(/^0\.$/)
-})
+  describe('digits', () => {
+    test('prevents leading zeros', () => {
+      fireEvent.click(keyPad.ZERO)
+      fireEvent.click(keyPad.ONE)
 
-test('uses last operator clicked', () => {
-  fireEvent.click(keyPad.ONE)
-  fireEvent.click(keyPad.MULTIPLY)
-  fireEvent.click(keyPad.ADD)
+      expect(display.EXPRESSION).toHaveTextContent(/^1$/)
+      expect(display.INPUT).toHaveTextContent(/^1$/)
+    })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^1\+$/)
-  expect(display.INPUT).toHaveTextContent(/^\+$/)
-})
+    test('prevents multiple decimals', () => {
+      fireEvent.click(keyPad.DECIMAL)
+      fireEvent.click(keyPad.DECIMAL)
 
-test('uses negative on operator followed by minus', () => {
-  fireEvent.click(keyPad.ONE)
-  fireEvent.click(keyPad.MULTIPLY)
-  fireEvent.click(keyPad.SUBTRACT)
+      expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
+      expect(display.INPUT).toHaveTextContent(/^0\.$/)
+    })
 
-  expect(display.EXPRESSION).toHaveTextContent(/^1\*-$/)
-  expect(display.INPUT).toHaveTextContent(/^-$/)
-})
+    test('prepends decimal with zero', () => {
+      fireEvent.click(keyPad.DECIMAL)
 
-test('uses subtraction on negative followed by minus', () => {
-  fireEvent.click(keyPad.ONE)
-  fireEvent.click(keyPad.MULTIPLY)
-  fireEvent.click(keyPad.SUBTRACT)
-  fireEvent.click(keyPad.SUBTRACT)
+      expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
+      expect(display.INPUT).toHaveTextContent(/^0\.$/)
 
-  expect(display.EXPRESSION).toHaveTextContent(/^1-$/)
-  expect(display.INPUT).toHaveTextContent(/^-$/)
+      fireEvent.click(keyPad.ADD)
+      fireEvent.click(keyPad.DECIMAL)
+
+      expect(display.EXPRESSION).toHaveTextContent(/^0\.\+0\.$/)
+      expect(display.INPUT).toHaveTextContent(/^0\.$/)
+
+      fireEvent.click(keyPad.EQUALS)
+      fireEvent.click(keyPad.DECIMAL)
+
+      expect(display.EXPRESSION).toHaveTextContent(/^0\.$/)
+      expect(display.INPUT).toHaveTextContent(/^0\.$/)
+    })
+  })
 })
