c4b1945baec47ae32bec86547569a8ac50ce9b53
slipskid <2773262@gmail.com>
2020-02-05 23:43:31 -0500

    Replace it with test

--- src/modules/calculate.test.js
+++ src/modules/calculate.test.js
@@ -1,46 +1,46 @@
 import calculate from './calculate'
 
-it('returns a positive number', () => {
+test('returns a positive number', () => {
   expect(calculate('1=')).toEqual(1)
 })
 
-it('returns a negative number', () => {
+test('returns a negative number', () => {
   expect(calculate('-1=')).toEqual(-1)
 })
 
-it('adds two numbers', () => {
+test('adds two numbers', () => {
   expect(calculate('1+2=')).toEqual(3)
 })
 
-it('subtracts two numbers', () => {
+test('subtracts two numbers', () => {
   expect(calculate('3-2=')).toEqual(1)
 })
 
-it('multiplies two numbers', () => {
+test('multiplies two numbers', () => {
   expect(calculate('1*2=')).toEqual(2)
 })
 
-it('divides two numbers', () => {
+test('divides two numbers', () => {
   expect(calculate('4/2=')).toEqual(2)
 })
 
-it('follows algebraic operator precedence', () => {
+test('follows algebraic operator precedence', () => {
   expect(calculate('2+2*2-2/2=')).toEqual(5)
 })
 
-it('throws on divide by zero', () => {
+test('throws on divide by zero', () => {
   expect(() => {
     calculate('1/0=')
   }).toThrowError(new Error('divide by zero'))
 })
 
-it('throws on invalid primary', () => {
+test('throws on invalid primary', () => {
   expect(() => {
     calculate('x=')
   }).toThrowError(new Error('primary expected'))
 })
 
-it('throws on bad symbols', () => {
+test('throws on bad symbols', () => {
   expect(() => {
     calculate('2?2=')
   }).toThrowError(new Error('expression error'))
