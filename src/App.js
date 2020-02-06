671b8b9f09eaec9646f2af628a1e6e5558de327a
slipskid <2773262@gmail.com>
2020-02-06 16:22:09 -0500

    Reformat #handleOperator

--- src/App.js
+++ src/App.js
@@ -82,12 +82,17 @@ export default class App extends React.Component {
   handleOperator = (state, operator) => {
     const { isEquals, isNegative, isOperator } = state
 
-    if (isEquals) return Operator.appendToResult(state, operator)
-    else if (isNegative) return Operator.replaceNegative(state, operator)
-    else if (isOperator) return (operator === '-')
-      ? Operator.appendNegative(state, operator)
-      : Operator.replaceOperator(state, operator)
-    else return Operator.append(state, operator)
+    if (isEquals) {
+      return Operator.appendToResult(state, operator)
+    } else if (isNegative) {
+      return Operator.replaceNegative(state, operator)
+    } else if (isOperator) {
+      return (operator === '-')
+        ? Operator.appendNegative(state, operator)
+        : Operator.replaceOperator(state, operator)
+    } else {
+      return Operator.append(state, operator)
+    }
   }
 
   handleDecimal = (decimal) => {
