c47c24db7be033001dfc1fe83a6dbc406e8a7f04
slipskid <2773262@gmail.com>
2020-02-06 20:52:52 -0500

    Move functions from equals.js to handle-equals.js

--- src/callbacks/equals.js
+++ src/callbacks/equals.js
@@ -1,20 +1,6 @@
 import setPrecision10 from '../modules/setPrecision10'
 import calculate from '../modules/calculate'
 
-export const replaceNegative = (state, equals) => ({
-  expression: state.expression.slice(0, -2) + equals,
-  isNegative: !state.isNegative
-})
-
-export const replaceOperator = (state, equals) => ({
-  expression: state.expression.slice(0, -1) + equals,
-  isOperator: !state.isOperator
-})
-
-export const append = (state, equals) => ({
-  expression: state.expression + equals
-})
-
 export const result = state => {
   let result
 
--- src/handlers/handle-equals.js
+++ src/handlers/handle-equals.js
@@ -1,13 +1,25 @@
-import * as Equals from '../callbacks/equals'
-
 export default function handleEquals (state, equals) {
   const { isNegative, isOperator } = state
 
   if (isNegative) {
-    return Equals.replaceNegative(state, equals)
+    return replaceNegative(state, equals)
   } else if (isOperator) {
-    return Equals.replaceOperator(state, equals)
+    return replaceOperator(state, equals)
   } else {
-    return Equals.append(state, equals)
+    return append(state, equals)
   }
 }
+
+export const replaceNegative = (state, equals) => ({
+  expression: state.expression.slice(0, -2) + equals,
+  isNegative: !state.isNegative
+})
+
+export const replaceOperator = (state, equals) => ({
+  expression: state.expression.slice(0, -1) + equals,
+  isOperator: !state.isOperator
+})
+
+export const append = (state, equals) => ({
+  expression: state.expression + equals
+})
