bbd429eae56eeeef99b9db89508713de5c683a56
slipskid <2773262@gmail.com>
2020-02-07 09:31:49 -0500

    Move functions from operator.js to handle-operator.js

--- src/handlers/handle-operator.js
+++ src/handlers/handle-operator.js
@@ -1,17 +1,47 @@
-import * as Operator from '../callbacks/operator'
-
 export default function handleOperator (state, operator) {
   const { isEquals, isNegative, isOperator } = state
 
   if (isEquals) {
-    return Operator.appendToResult(state, operator)
+    return appendToResult(state, operator)
   } else if (isNegative) {
-    return Operator.replaceNegative(state, operator)
+    return replaceNegative(state, operator)
   } else if (isOperator) {
     return (operator === '-')
-      ? Operator.appendNegative(state, operator)
-      : Operator.replaceOperator(state, operator)
+      ? appendNegative(state, operator)
+      : replaceOperator(state, operator)
   } else {
-    return Operator.append(state, operator)
+    return append(state, operator)
   }
 }
+
+const appendToResult = (state, operator) => ({
+  expression: state.input + operator,
+  input: operator,
+  isEquals: !state.isEquals,
+  isOperator: !state.isOperator
+})
+
+const replaceNegative = (state, operator) => ({
+  expression: state.expression.slice(0, -2) + operator,
+  input: operator,
+  isNegative: !state.isNegative,
+  isOperator: !state.isOperator
+})
+
+const appendNegative = (state, operator) => ({
+  expression: state.expression + operator,
+  input: operator,
+  isNegative: !state.isNegative,
+  isOperator: !state.isOperator
+})
+
+const replaceOperator = (state, operator) => ({
+  expression: state.expression.slice(0, -1) + operator,
+  input: operator
+})
+
+const append = (state, operator) => ({
+  expression: state.expression + operator,
+  input: operator,
+  isOperator: !state.isOperator
+})
