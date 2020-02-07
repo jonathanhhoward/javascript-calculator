194f657425818d5ddc301ecac1909d594b22b210
slipskid <2773262@gmail.com>
2020-02-06 19:15:19 -0500

    Move contents of delete.js to handle-delete.js

--- src/callbacks/delete.js
+++
@@ -1,4 +0,0 @@
-export const zeroInput = state => ({
-  expression: state.expression.slice(0, -state.input.length) + '0',
-  input: '0'
-})
--- src/handlers/handle-delete.js
+++ src/handlers/handle-delete.js
@@ -1,9 +1,12 @@
-import * as Delete from '../callbacks/delete'
-
 export default function handleDelete (state) {
   const { isEquals, isNegative, isOperator } = state
 
   if (isEquals || isNegative || isOperator) return state
 
-  return Delete.zeroInput(state)
+  return zeroInput(state)
 }
+
+const zeroInput = (state) => ({
+  expression: state.expression.slice(0, -state.input.length) + '0',
+  input: '0'
+})
