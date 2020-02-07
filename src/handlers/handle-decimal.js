837f690f847dfabbdab66ac6bda5173d01640520
slipskid <2773262@gmail.com>
2020-02-07 14:21:46 -0500

    Add return state to guard clause

--- src/handlers/handle-decimal.js
+++ src/handlers/handle-decimal.js
@@ -3,7 +3,7 @@ import handleDigit from './handle-digit'
 export default function handleDecimal (state, decimal) {
   const { input, isResult, isNegative, isOperator } = state
 
-  if (input.includes(decimal) && !isResult) return
+  if (input.includes(decimal) && !isResult) return state
 
   return (input === '0' || isResult || isNegative || isOperator)
     ? handleDigit(state, '0' + decimal)
