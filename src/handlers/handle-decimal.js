b78ab5fd68a35fb559949650e698ea0eef774122
slipskid <2773262@gmail.com>
2020-02-07 11:00:07 -0500

    Change isEquals to isResult

--- src/handlers/handle-decimal.js
+++ src/handlers/handle-decimal.js
@@ -1,11 +1,11 @@
 import handleDigit from './handle-digit'
 
 export default function handleDecimal (state, decimal) {
-  const { input, isEquals, isNegative, isOperator } = state
+  const { input, isResult, isNegative, isOperator } = state
 
-  if (input.includes(decimal) && !isEquals) return
+  if (input.includes(decimal) && !isResult) return
 
-  return (input === '0' || isEquals || isNegative || isOperator)
+  return (input === '0' || isResult || isNegative || isOperator)
     ? handleDigit(state, '0' + decimal)
     : handleDigit(state, decimal)
 }
