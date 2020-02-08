5757ea34d6bfe1928a16ed19fbf9adc7fc707329
slipskid <2773262@gmail.com>
2020-02-08 09:17:18 -0500

    Move keys to keys.js

---
+++ src/modules/keys.js
@@ -0,0 +1,22 @@
+export default function keys () {
+  return [
+    { id: 'clear', class: 'clear', value: 'AC' },
+    { id: 'delete', class: 'delete', value: 'C' },
+    { id: 'divide', class: 'operator', value: '/' },
+    { id: 'multiply', class: 'operator', value: '*' },
+    { id: 'subtract', class: 'operator', value: '-' },
+    { id: 'add', class: 'operator', value: '+' },
+    { id: 'equals', class: 'equals', value: '=' },
+    { id: 'decimal', class: 'digit', value: '.' },
+    { id: 'zero', class: 'digit', value: '0' },
+    { id: 'one', class: 'digit', value: '1' },
+    { id: 'two', class: 'digit', value: '2' },
+    { id: 'three', class: 'digit', value: '3' },
+    { id: 'four', class: 'digit', value: '4' },
+    { id: 'five', class: 'digit', value: '5' },
+    { id: 'six', class: 'digit', value: '6' },
+    { id: 'seven', class: 'digit', value: '7' },
+    { id: 'eight', class: 'digit', value: '8' },
+    { id: 'nine', class: 'digit', value: '9' }
+  ]
+}
