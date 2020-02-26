d7253de10cb4739a0d74ffe6e5dfe0adbfc39d95
slipskid <2773262@gmail.com>
2020-02-26 15:17:54 -0500

    Add testids for expression and input

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,8 +4,12 @@ import './Display.css'
 export default function Display (props) {
   return (
     <div className="Display">
-      <div className="Expression">{props.expression}</div>
-      <div id="display" className="Input">{props.input}</div>
+      <div className="Expression" data-testid="expression">
+        {props.expression}
+      </div>
+      <div id="display" className="Input" data-testid="input">
+        {props.input}
+      </div>
     </div>
   )
 }
