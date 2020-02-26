db4a4277724cec96367bf9af24918338361ecc64
slipskid <2773262@gmail.com>
2020-02-26 15:37:56 -0500

    Changes input only except clears expression after result

--- src/handlers/handle-digit.js
+++ src/handlers/handle-digit.js
@@ -1,39 +1,36 @@
 export default function handleDigit (state, digit) {
   const { input, isResult, isOperator } = state
 
-  if (isMaxDigits(input,10) && !isResult) return state
+  if (isMaxDigits(10)) return state
 
-  if (isResult) return replaceResult(state, digit)
+  if (isResult) return resetResultReplaceInput(state, digit)
 
-  if (isOperator) return appendToOperator(state, digit)
+  if (isOperator) return resetOperatorReplaceInput(state, digit)
 
-  if (input === '0') return replaceZero(state, digit)
+  if (input === '0') return replaceInput(state, digit)
 
-  return append(state, digit)
+  return appendToInput(state, digit)
 
-  function isMaxDigits (input, limit) {
-    return (input.replace(/[.-]/g, '').length === limit)
+  function isMaxDigits (limit) {
+    return (input.replace(/[.-]/g, '').length === limit) && !isResult
   }
 }
 
-const replaceResult = (state, digit) => ({
-  expression: digit,
+const resetResultReplaceInput = (state, digit) => ({
+  expression: '',
   input: digit,
   isResult: !state.isResult
 })
 
-const appendToOperator = (state, digit) => ({
-  expression: state.expression + digit,
+const resetOperatorReplaceInput = (state, digit) => ({
   input: digit,
   isOperator: !state.isOperator
 })
 
-const replaceZero = (state, digit) => ({
-  expression: state.expression.slice(0, -1) + digit,
+const replaceInput = (state, digit) => ({
   input: digit
 })
 
-const append = (state, digit) => ({
-  expression: state.expression + digit,
+const appendToInput = (state, digit) => ({
   input: state.input + digit
 })
