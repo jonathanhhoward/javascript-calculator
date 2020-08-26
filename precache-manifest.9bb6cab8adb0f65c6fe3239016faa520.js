5aa7cb048d48cae8e00ec6084d50d5650a8b4f72
jonathanhhoward <2773262@gmail.com>
2020-08-25 20:48:29 -0400

    Updates

---
+++ precache-manifest.9bb6cab8adb0f65c6fe3239016faa520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a3b53a8fece189001554ec2a24a023",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "260db6ad73f5873376c2",
+    "url": "/javascript-calculator/static/css/main.8d44f4a3.chunk.css"
+  },
+  {
+    "revision": "8769be7449e4c734bbcb",
+    "url": "/javascript-calculator/static/js/2.4924a353.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.4924a353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260db6ad73f5873376c2",
+    "url": "/javascript-calculator/static/js/main.7b741f65.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
