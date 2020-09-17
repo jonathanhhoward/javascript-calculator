eb33c7a49ba00d537d80cf31b5f8ba5b882a082d
jonathanhhoward <2773262@gmail.com>
2020-09-17 00:18:33 -0500

    Replace function with variable

--- src/handlers/handleDigit.js
+++ src/handlers/handleDigit.js
@@ -1,8 +1,9 @@
 function handleDigit(state, digit) {
   const { input } = state;
+  const isMaxDigits = input.replace(/[.-]/g, '').length === 10;
   const isResult = state.status === 'RESULT';
 
-  if (isMaxDigits(input, 10) && !isResult) return state;
+  if (isMaxDigits && !isResult) return state;
 
   switch (state.status) {
     case 'RESULT':
@@ -13,10 +14,6 @@ function handleDigit(state, digit) {
       return appendToOperator(state, digit);
     default:
       return input === '0' ? replaceZero(state, digit) : append(state, digit);
-  }
-
-  function isMaxDigits(input, limit) {
-    return input.replace(/[.-]/g, '').length === limit;
   }
 }
 
